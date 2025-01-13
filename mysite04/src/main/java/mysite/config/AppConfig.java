72b65ac4b31b7c4291a7a9243c65dcc83853c54f
seowonn <sonseowon5@gmail.com>
2025-01-13 11:53:17 +0900

    Refactor: xml to java
    - applicationContext.xml의 annotation-config를 java의 AppConfig클래스로 변경

--- mysite04/src/main/java/mysite/config/AppConfig.java
+++ mysite04/src/main/java/mysite/config/AppConfig.java
@@ -1,5 +1,10 @@
 package mysite.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@ComponentScan(basePackages = {"mysite.service, mysite.repository, mysite.aspect"})
 public class AppConfig {
 
 }
