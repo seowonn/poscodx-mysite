8c5dce176eda801e24be137941151fc4b9d79035
seowonn <sonseowon5@gmail.com>
2025-01-14 13:06:32 +0900

    Refactor: web.xml to Java classes
    - fileupload.properties

--- mysite05/src/main/java/mysite/initializer/MySiteWebApplicationInitializer.java
+++ mysite05/src/main/java/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -1,26 +1,45 @@
 package mysite.initializer;
 
+import java.util.ResourceBundle;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration.Dynamic;
+import mysite.config.AppConfig;
+import mysite.config.WebConfig;
 
 public class MySiteWebApplicationInitializer
 		extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+	/**
+	 * Root Application Context을 생성하여 반환하는 곳 이어서 ContextLoad Listener를 실행
+	 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Class<?>[] { AppConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	// web.xml의 <servlet-mapping> 태그 역할
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
 	}
 
 	@Override
-	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
-		return null;
+	protected void customizeRegistration(Dynamic registration) {
+		ResourceBundle bundle = ResourceBundle.getBundle("mysite.config.web.fileupload");
+		long maxFileSize = Long.parseLong(bundle.getString("fileupload.maxFileSize"));
+		long maxRequestSize = Long.parseLong(bundle.getString("fileupload.maxRequestSize"));
+		int fileSizeThreshold = Integer.parseInt(bundle.getString("fileupload.fileSizeThreshold"));
+		MultipartConfigElement config = new MultipartConfigElement(null, maxFileSize,
+				maxRequestSize, fileSizeThreshold);
+		registration.setMultipartConfig(config);
 	}
 
 }
